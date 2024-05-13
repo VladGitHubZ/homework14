e53e5c2f9a02152f937ae65a7bd5a663f4e7f8d8
Vladislav Zinziuk <2962266@mail.ru>
2024-05-13 18:22:47 +0300

    homework14 OOP2

---
+++ src/Bicycle.java
@@ -0,0 +1,6 @@
+public class Bicycle extends Transport{
+    private static final int WHEELS_COUNT = 2;
+    public Bicycle(String modelName) {
+        super(modelName, WHEELS_COUNT);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,23 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Bicycle bicycle = new Bicycle("Bicycle 1");
+        Car car = new Car("Car 1", 4);
+        Truck truck = new Truck("Truck 1", 6);
+        ServiceStation serviceStation = new ServiceStation();
+
+        System.out.println("Bicycle diagnostic");
+        serviceStation.check(bicycle);
+        System.out.println("------------------");
+
+        System.out.println("Car diagnostic");
+        serviceStation.check(car);
+        System.out.println("------------------");
+
+        System.out.println("Truck diagnostic");
+        serviceStation.check(truck);
+        System.out.println("------------------");
+
 
     }
 }
